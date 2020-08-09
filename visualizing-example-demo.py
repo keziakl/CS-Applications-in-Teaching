08c50b6fd856552ed015344528c54eaf7c7fb6be
Kezia Lopez <keziakl@stanford.edu>
2020-08-09 17:24:16 -0500

    addition of teaching files

---
+++ visualizing-example-demo.py
@@ -0,0 +1,21 @@
+'''
+#Example 1: Java
+int numBagsInCar = 14;
+for (int i = 0; i < 7; i++)
+    numBagsInCar = numBagsInCar - 2;
+
+#Example 1: Python
+num_bags_in_car = 14
+for x in range(7):
+    num_bags_in_car = num_bags_in_car - 2
+
+#Example 2: Java
+int numBagsInCar = 14;
+while (numBagsInCar > 0)
+    numBagsInCar = numBagsInCar - 2;
+
+#Example 2: Python
+num_bags_in_car = 14
+while num_bags_in_car > 0:
+    num_bags_in_car = num_bags_in_car - 2
+
